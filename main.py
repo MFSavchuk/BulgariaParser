1f8d552aa8e0b11a9064fe1804e562974145ce34
MikhailSavchuk2 <mr.savchuk@bk.ru>
2022-12-26 17:33:01 +0300

    инит

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import multiprocessing
+from time import sleep
 
 from utils.time_track import time_track
 from utils.create_lists_and_new_file_for_parser import create_lists_and_new_file_for_parser
@@ -14,34 +15,40 @@ def run_parser(multiproc):
     parsers = [Parser(statements=statements, collector=collector) for statements in lists]
     for parser in parsers:
         parser.start()
-    for parser in parsers:
-        parser.join()
+
+    sleep(30)
+
     with open(new_file, 'a', encoding='utf8') as ff:
-        while not collector.empty():
-            print(f'Длина очереди - {collector.qsize()} ')
+        while True:
+            one_is_alive = any([parser.is_alive() for parser in parsers])
+            # print([parser.is_alive() for parser in parsers])
+            if one_is_alive:
+                if not collector.empty():
+                    data = str(collector.get())
+                    ff.write(data)
+                    print(f'Строка {data.split(" ", 1)[0]} записана')
+                    # print(f'Длина очереди - {collector.qsize()} ')
+                    # sleep(0.4)
+            else:
+                break
+
+        print('Этап 1 пройден')
+        print(f'Длина очереди - {collector.qsize()} ')
+
+        if not collector.empty():
             data = str(collector.get())
             ff.write(data)
             print(f'Строка {data.split(" ", 1)[0]} записана')
+
+        print('Этап 2 пройден')
+
+    for parser in parsers:
+        parser.join()
+
+    print('Этап 3 пройден')
+
     sort_file(new_file)
     compare(file_result='compare_result.txt')
-    # sleep(30)
-    #
-    # with open(new_file, 'a', encoding='utf8') as ff:
-    #     while True:
-    #         one_is_alive = any([parser.is_alive() for parser in parsers])
-    #         # print([parser.is_alive() for parser in parsers])
-    #         if one_is_alive:
-    #             if not collector.empty():
-    #                 data = str(collector.get())
-    #                 ff.write(data)
-    #                 print(f'Строка {data.split(" ", 1)[0]} записана')
-    #                 # print(f'Длина очереди - {collector.qsize()} ')
-    #                 # sleep(0.4)
-    #         else:
-    #             break
-    #
-    #     print('проеден')
-    #     print(f'Длина очереди - {collector.qsize()} ')
 
 
 if __name__ == '__main__':
